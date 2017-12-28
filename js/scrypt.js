eb63150fae61b31d06683b4b865e7a94dad04e3c
Oleg Dubrovsky <olegdubrowsky@gmail.com>
2017-12-28 16:28:52 +0200

    fixedOleg

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('.menu').on('click',  function(e){
 		e.preventDefault();
-		$('.navLink').addClass('active');
-		}
-	})
-});
+		$('.navLink').toggleClass('active');
+		});
+	});
